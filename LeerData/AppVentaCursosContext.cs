e552f76209cc59e5df0b3deea4b795eae095a8ad
Juan Manuel Calderon Robles <sayroback@gmail.com>
2022-10-31 20:41:23 -0600

    first commit

---
+++ LeerData/AppVentaCursosContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LeerData
+{
+  public class AppVentaCursosContext : DbContext
+  {
+    private const string connectionString = @"Data Source=DESKTOP-JBICHIM\HALLEYSQL;Initial Catalog=CursosOnline;User ID=sa;Password=3242;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlServer(connectionString);
+    }
+    public DbSet<Curso> Curso { get; set; }
+  }
+}
---
+++ LeerData/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace LeerData;
+class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.WriteLine("Hello, World! XD");
+  }
+}
