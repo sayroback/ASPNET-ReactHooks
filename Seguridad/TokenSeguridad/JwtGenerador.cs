2504e67fb0d5991fefc08e6ad87d1b2da6f701d4
Juan Manuel Calderon Robles <sayroback@gmail.com>
2022-11-22 15:37:38 -0600

    Creación del Token

--- Seguridad/TokenSeguridad/JwtGenerador.cs
+++ Seguridad/TokenSeguridad/JwtGenerador.cs
@@ -1,11 +1,34 @@
 ﻿using Aplicacion.Contratos;
 using Dominio;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
 
 namespace Seguridad.TokenSeguridad;
 public class JwtGenerador : IJwtGenerador
 {
   public string GenerateToken(Usuario usuario)
   {
-    throw new NotImplementedException();
+    var claims = new List<Claim>
+    {
+      new Claim(JwtRegisteredClaimNames.NameId, usuario.UserName)
+    };
+
+    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Palabra Secreta 3242 zxc"));
+    var credenciales = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+    var tokenDescripcion = new SecurityTokenDescriptor
+    {
+      Subject = new ClaimsIdentity(claims),
+      Expires = DateTime.Now.AddDays(10),
+      SigningCredentials = credenciales
+    };
+
+    var tokenManejador = new JwtSecurityTokenHandler();
+    var token = tokenManejador.CreateToken(tokenDescripcion);
+
+    return tokenManejador.WriteToken(token);
   }
 }
